844292136d8f258bc0a1ff5afe55de1b6959f16f
FernandaCidade <fernanda.cidade@outlook.com>
2024-02-29 19:01:17 -0300

    API gerada 29-02

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# API de Conversão de Moeda
+
+A API desenvolvida para realizar o cambio de moedas, realizando o processamento dentro da mesma.
+
+##  📋Pré-requisitos 
+```
+pip v24.0 
+python 3.7
+```
+### 🔩 Ferramentas utilizadas
+```
+VSCode
+Insomnia
+replit.com
+```
+
+## 🛠️Instalação das bibliotecas
+ Para utilização da API é necessário a instalação das seguintes bibliotecas:
+* [Flask](https://flask.palletsprojects.com/en/3.0.x/api/) - Framework utilizado
+* [Forex_python.converter](https://pypi.org/project/forex-python/) - Biblioteca utilizada para cambio e conversão de moeda 
+
+### Windows
+```
+pip install Flask
+pip install forex_python.converter
+```
+### Linux ubuntu
+```
+sudo apt install Flask
+sudo apt install forex_python.converter
+```
+
+## ⚙️Testes
+Projeto em fase inicial de testes. 
+
--- api/api.py
+++ api/api.py
@@ -1,55 +1,94 @@
+from flask import Flask, jsonify
 
-import requests
+  # Biblioteca de conversão de moedas
+from forex_python.converter import CurrencyCodes, CurrencyRates
+c = CurrencyRates()
+cd = CurrencyCodes()
 
-# Realizar conexão
-# Pegar o valor a ser convertido
-# Resposta do servidor e exibir 
+app = Flask(__name__)
+  # Titulo do programa
+
+@app.route('/cambio', methods=['POST'])
+
+# Função para escolher o codigo da moeda
+def valor(valor):
+  while True:
+      print ("CONVERSOR DE MOEDA")
+      print (" Insira o valor desejado para cambiar\n- Insira a moeda inicial \n- Insira a moeda para cambio")
+      valor = int(input('Insira o valor a ser convertido: '))
+      return jsonify(valor)
+
+# 2 Moeda inicial
+def moeda_inicio():
+              print('Selecione a moeda de inicio')
+              cod_moeda_inicio = None 
+              codigo = int(input("selecione uma das opções:  "))
+              match codigo:
+                  case 1:
+                      cod_moeda_inicio = 'BRL'
+                      print("cod de escolha:", cod_moeda_inicio)
+                      return cod_moeda_inicio
+                  case 2:
+                      cod_moeda_inicio = 'USD'
+                      print("cod de escolha:", cod_moeda_inicio)
+                      return cod_moeda_inicio
+                  case 3:
+                      cod_moeda_inicio = 'EUR'
+                      print("cod de escolha:", cod_moeda_inicio)
+                      return cod_moeda_inicio
+                  case 4:
+                      cod_moeda_inicio = 'BTC'
+                      print("cod de escolha:", cod_moeda_inicio)
+                      return cod_moeda_inicio
+                  case 5:
+                      cod_moeda_inicio = 'ETH'
+                      print("cod de escolha:", cod_moeda_inicio)
+                      return cod_moeda_inicio
+                  case _:
+                      raise ValueError("Código inválido! Favor inserir dentre as opções fornecidas.")
+          
+
+# 3 Moeda de cambio
+def moeda_cambio():
+  print('Selecione a moeda para cambio')
+  cod_moeda_cambio = None 
+  codigo = int(input("selecione uma das opções:  "))
+  match codigo:
+      case 1:
+          cod_moeda_cambio = 'BRL'
+          print("cod de escolha:", cod_moeda_cambio)
+          return cod_moeda_cambio
+      case 2:
+          cod_moeda_cambio = 'USD'
+          print("cod de escolha:", cod_moeda_cambio)
+          return cod_moeda_cambio
+      case 3:
+         cod_moeda_cambio = 'EUR'
+         print("cod de escolha:", cod_moeda_cambio)
+         return cod_moeda_cambio
+      case 4:
+          cod_moeda_cambio = 'BTC'
+          print("cod de escolha:", cod_moeda_cambio)
+          return cod_moeda_cambio
+      case 5:
+          cod_moeda_cambio = 'ETH'
+          print("cod de escolha:", cod_moeda_cambio)
+          return cod_moeda_cambio
+      case _:
+          raise ValueError("Código inválido! Favor inserir dentre as opções fornecidas.")
+    
+
+    # Informações do cambio 
 
 
+def cambio(cod_moeda_inicial, moeda_inicial, resultado, cod_moeda_cambio, moeda_cambio):
 
-# Realizando conexão com url
-def conexao():
-    url = "http://127.0.0.1:8000/moeda"
-    response = requests.get(url)
-    data = response.json()
+  resultado = c.convert(cod_moeda_inicial, cod_moeda_cambio, moeda_inicial)
+  simbolo_moeda = cd.get_symbol(cod_moeda_inicial)
+  simbolo_moeda_cambio = cd.get_symbol(cod_moeda_cambio)
+  resultado = round(resultado, 2)
 
-    return data['rates']
+  return jsonify(cod_moeda_inicial + str(moeda_inicial) + cod_moeda_cambio + str(resultado))
 
-
-# Realizando a conversão da moeda
-
-def conversao_moeda(moeda_origem,moeda_convertida, taxa, moedas):
-    # realizar a verificação da taxa a ser aplicada
-         
-         match moedas:
-            case "USD":
-                taxa = 4
-                moeda_convertida = moeda_origem * taxa
-                return moeda_convertida
-            case "ETH":
-                taxa = 15
-                moeda_convertida = moeda_origem * taxa
-                return moeda_convertida
-            case "BTC":
-                taxa = 19
-                moeda_convertida = moeda_origem * taxa
-                return moeda_convertida
-            case "BRL":
-                taxa = 19
-                moeda_convertida = moeda_origem * taxa
-                return moeda_convertida
-            case "EUR":
-                taxa = 20
-                moeda_convertida = moeda_origem * taxa
-                return moeda_convertida
-             
-            case default:
-                return None
-             
-cambio = conexao()
-moedas = input("Qual valor da conversão: ").upper()
-
-moeda_convertida = conversao_moeda(moeda_origem,moeda_convertida, taxa, moedas)
-
-if moeda_convertida is not None:
-    print(f'\n{moeda_convertida:.2f}')
+# rodar a API
+app.run(host='0.0.0.0')
